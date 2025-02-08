f444e208cfb0cd88b5abb63db5f9bd0369b80ae5
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-02-08 20:19:21 -0300

    commit test and prettier format

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-const express = require('express')
-const { getAllCars, getCarById, updateCar } = require('./db')
-const app = express()
-const port = 3000
-const db = require('./db')
+const express = require("express");
+const { getAllCars, getCarById, updateCar } = require("./db");
+const app = express();
+const port = 3000;
+const db = require("./db");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/cars', (req, res) => {
-  res.json(getAllCars())
-})
+app.get("/cars", (req, res) => {
+  res.json(getAllCars());
+});
 
-app.get('/car/:id', (req, res) => {
-    const id = +req.params.id
-    res.json(getCarById(id))
-})
+app.get("/car/:id", (req, res) => {
+  const id = +req.params.id;
+  res.json(getCarById(id));
+});
 
-app.post('/car', (req, res) => {
-    const car = req.body
-    db.createCar(car)
-    res.sendStatus(201)
-})
+app.post("/car", (req, res) => {
+  const car = req.body;
+  db.createCar(car);
+  res.sendStatus(201);
+});
 
-app.patch('/car/:id', (req, res) => {
-    const id = +req.params.id
-    const car = req.body
-    db.updateCar(id, car)
-    res.sendStatus(200)
-})
+app.patch("/car/:id", (req, res) => {
+  const id = +req.params.id;
+  const car = req.body;
+  db.updateCar(id, car);
+  res.sendStatus(200);
+});
 
-app.delete('/car/:id', (req, res) => {
-    const id = +req.params.id
-    db.deleteCar(id)
-    res.sendStatus(200)
-})
+app.delete("/car/:id", (req, res) => {
+  const id = +req.params.id;
+  db.deleteCar(id);
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`)
-})
+  console.log(`Running on port ${port}`);
+});
