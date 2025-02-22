14d72583d627431e648d24ca6bc74d29963645a3
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-02-22 16:11:47 -0300

    chore: adding cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 const db = require("./db");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/cars", async (req, res) => {
   const results = await db.getAllCars();
